21c83b9cd762782d37022074b9e13cec347bea0c
olegserbat <olegserbat@yandex.ru>
2024-01-13 20:33:44 +0300

    step 9

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Step 5. link to the operation of the game gameCode      [![asciicast](https://as
 Step 6. link to the operation of the game Game-Calc  [![asciicast](https://asciinema.org/a/ksLiRguWw7HQEgAajFe3iPANo.svg)](https://asciinema.org/a/ksLiRguWw7HQEgAajFe3iPANo)
 Step 7. link to the operation of the game Game-gcd [![asciicast](https://asciinema.org/a/yrZvU3J9tHXaD7LYmjEEZnsjm.svg)](https://asciinema.org/a/yrZvU3J9tHXaD7LYmjEEZnsjm)
 Step 8. link to the operation of the game Game-Progressive [![asciicast](https://asciinema.org/a/R3fTmFaZSwLR0R1veRlvBXZVp.svg)](https://asciinema.org/a/R3fTmFaZSwLR0R1veRlvBXZVp)
+Step 9. link to the operation of the game Game-Prime  [![asciicast](https://asciinema.org/a/ZuGkCo8TTfGLqvfJ3POd4q3ME.svg)](https://asciinema.org/a/ZuGkCo8TTfGLqvfJ3POd4q3ME)
---
+++ src/PrimeGame.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace BrainGames\Cli;
+function giveDescriptionsForPrime(): string
+{
+    return 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+}
+
+function giveArrayForPrime(): array
+{
+    $result = [];
+    while (count($result) < 3) {
+        foreach (getPrime() as $key => $value) {
+            $result[$key] = $value;
+        }
+    }
+    return $result;
+}
+
+function getPrime(): array
+{
+    $a = rand(2, 100);
+    $result = [];
+    if (in_array($a, getPrimeNumber())) {
+        $result[$a] = 'yes';
+    } else {
+        $result[$a] = 'no';
+    }
+    return $result;
+}
+
+function getPrimeNumber(): array // получение массива простых чисел по методу Эратосфена
+{
+    $n = 100;// предел массива
+    $arrOne = [];
+    $result = [];
+    $arrTwo = [];
+    for ($i = 2; $i < $n; $i++) {
+        $arrOne[] = $i;
+    }
+    $j = 0;
+    while ($j < count($arrOne)) {
+        $result[] = $arrOne[0];
+        foreach ($arrOne as $value) {
+            if ($value % ($arrOne[0]) != 0) {
+                $arrTwo[] = $value;
+            }
+        }
+        $arrOne = $arrTwo;
+        $arrTwo = [];
+    }
+    return $result;
+}
+
