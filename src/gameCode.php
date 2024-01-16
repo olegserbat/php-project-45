8119f5c0dbf78e4a272a09df02f293d4df018ff9
olegserbat <olegserbat@yandex.ru>
2024-01-16 22:03:33 +0300

    step 9.3 correct mistake with  word Games

--- src/gameCode.php
+++ src/gameCode.php
@@ -7,7 +7,7 @@ use function cli\prompt;
 
 function getDialog( string $description, array $answersQuestions  )
 {
-    line('Welcome to the Brain Game!');
+    line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
     line($description);
